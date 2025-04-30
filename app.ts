2299f67967a72b6d294f2eafd18e8137498a25c8
FelipeLohan <felipelohan03052006@gmail.com>
2025-04-30 17:00:22 -0300

    Added router path

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const scrumSquadRouter = require('./routes/scrum-squad-routes.js');
 
 app.use(express.json());
 app.use(cors({origin: "*"}))
-
 const port = 8000;
 
-
+app.use('/', scrumSquadRouter);
 
 app.listen(port, () => console.log(`Listening in port: ${port}`));
